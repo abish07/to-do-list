4060bdd133f2acfe0b85c0194b234e6fb0dee553
abish07 <abishshrestha24@gmail.com>
2025-06-02 19:24:22 +0545

    first commit on the project todo list site where we built a basic concept on adding any task deleting and saving it in a localstorage dynamically

---
+++ script.js
@@ -0,0 +1,45 @@
+let add = document.querySelector("#add");
+let addTask = document.querySelector(".addTask");
+let taskList = document.querySelector(".taskList")
+
+let counter=1;
+
+add.addEventListener("click", ()=>{
+    let taskId = "Task" + counter;
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+
+    let label = document.createElement("label");
+    label.textContent="Task: " + counter;
+    counter++;
+
+    let input = document.createElement("input");
+    input.type="text";
+    input.placeholder="Enter the Task";
+    
+    let save = document.createElement("button");
+    save.textContent="save";
+    save.addEventListener("click", ()=>{
+        let inputTask = input.value;
+        if(inputTask.trim()!== ""){
+            localStorage.setItem(taskId, inputTask);
+            head.textContent = inputTask;
+        }
+    })
+    let head = document.createElement("h1");
+    let remove = document.createElement("button");
+    remove.textContent="Delete";
+
+    remove.addEventListener("click", ()=>{
+        localStorage.removeItem(taskId);
+        wrapper.remove();
+    })
+    wrapper.appendChild(label);
+    wrapper.appendChild(input);
+    wrapper.appendChild(save);
+    wrapper.appendChild(remove);
+    wrapper.appendChild(head);
+
+    taskList.appendChild(wrapper);
+    input.focus();
+})
